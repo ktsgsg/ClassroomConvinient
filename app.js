fc00d307602a8803b3e655cdb5dc9c9ef9cbf341
Keita <184593592+ktsgsg@users.noreply.github.com>
2025-04-21 09:37:22 +0900

    change port

--- app.js
+++ app.js
@@ -9,4 +9,4 @@ app.get('/', (req, res) => {
 })
 
 //サーバーを起動したら、リクエストを8000番ポートで待ち受ける設定。
-app.listen(8000, () => console.log('Example app listening on port 8000!'))
+app.listen(3000, () => console.log('Example app listening on port 3000!!!!'))
